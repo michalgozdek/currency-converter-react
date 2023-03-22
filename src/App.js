2b0691ad06c23d0bcae9651e67f4582d60e6c5c3
michał gozdek <michalgozdek12@gmail.com>
2023-03-22 10:45:18 +0100

    add functionality

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import Header from './Header';
 import Container from './Container';
 import Footer from './Footer';
 import Form from './Forrm';
+import { useState } from 'react';
+import { currencies } from './currencies';
 
 function App() {
 
+  const [result, setResult]= useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({short}) => short ===Currency).rate;
+
+    setResult({
+     baseAmount: +amount,
+     targetAmount: amount / rate,
+     currency,
+    });
+  }
+
   return (
    
-      
-       
        <Container>
          <Header />
-          <Form />
+          <Form 
+           result={result}
+           calculateResult={calculateResult}
+          />
           <Footer />
        </Container>
-        
-         
-
-
-
-       
-    
-
-
   );
 
 };
---
+++ src/currencies.js
@@ -0,0 +1,12 @@
+export const currencies =[
+    {
+        name: "Dolar",
+        short: "USD",
+        rate: 4.41,
+    },
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.74,
+    },
+];
