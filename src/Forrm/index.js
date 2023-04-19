b4bff81749a79d6443e35e74ab7ca472f6383308
michał gozdek <michalgozdek12@gmail.com>
2023-04-19 18:17:42 +0200

    add styled component to form

--- src/Forrm/index.js
+++ src/Forrm/index.js
@@ -1,60 +1,68 @@
-import { useState } from "react"
-import { currencies } from "../currencies"
-import "./style.css"
-import {Result} from "../Result";
+import { useState } from "react";
+import { currencies } from "../currencies";
+import { Result } from "../Result";
+import {
+  Field,
+  Legend,
+  Label,
+  Options,
+  Button,
+  Paragraph,
+  Effect,
+} from "./styled";
 
-export const Form = ({ calculateResult, result }) =>{
-    const [currency, setCurrency] = useState(currencies[0].short);
-    const [amount, setAmount] = useState("");
+export const Form = ({ calculateResult, result }) => {
+  const [currency, setCurrency] = useState(currencies[0].short);
+  const [amount, setAmount] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(currency , amount);
-    }
-   return(
-    <form className="form" onSubmit={onFormSubmit}>
-    <fieldset>
-        <legend className="form__header">Kalkulator</legend>
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
+  return (
+    <Field onSubmit={onFormSubmit}>
+      <fieldset>
+        <Legend className="form__header">Kalkulator</Legend>
         <p>
-            <label><span className="form__label"> Podaj kwotę w PLN </span>
+          <label>
+            <Label> Podaj kwotę w PLN </Label>
             <input
-             type="number" 
-             name="PLN"
-             min= "1"
-             placeholder="Wpisz kwotę w zł"
-             value={ amount }
-             onChange = {({target}) => setAmount(target.value)}
-             required 
-             /></label>
+              type="number"
+              name="PLN"
+              min="1"
+              placeholder="Wpisz kwotę w zł"
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              required
+            />
+          </label>
         </p>
         <p>
-            <label>
-                <span className="form__label">Wybierz walutę</span>
-                <select 
-                className="form__field"
-                 name="Curency"
-                 value={currency}
-                 onChange = {({ target }) => setCurrency (target.value)}>
-                    {currencies.map((currency) => (
-                        <option
-                        key={currency.short}
-                        value={currency.short}>
-                            {currency.name}
-                        </option>
-                    ))}
-                </select>
-
-            </label>
+          <label>
+            <Label>Wybierz walutę</Label>
+            <Options
+              name="Curency"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.short} value={currency.short}>
+                  {currency.name}
+                </option>
+              ))}
+            </Options>
+          </label>
         </p>
-    </fieldset>
-    <p className="buttons">
-       <button className="buttons__calculateInside">Oblicz</button>   
-    </p>
-    <p className="form__result">Kwota po Przeliczeniu :
-     <Result result={result} /> </p>
-</form>
-   );
-
+      </fieldset>
+      <Paragraph>
+        <Button>Oblicz</Button>
+      </Paragraph>
+      <Effect className="form__result">
+        Kwota po Przeliczeniu :
+        <Result result={result} />{" "}
+      </Effect>
+    </Field>
+  );
 };
 
 export default Form;
